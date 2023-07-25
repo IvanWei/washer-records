1bbcd8d2cc48fbda43df5bb8e56e2f396d62f696
Ivan Wei <scorpionseed@gmail.com>
2023-07-25 20:11:29 +0800

    style(type): update types

---
+++ src/types/index.d.ts
@@ -0,0 +1,5 @@
+export type INFOS_TYPES = {
+  washers: { label: string; value: string }[];
+  types: { label: string; value: string }[];
+  whos: { label: string; value: string }[];
+};
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,10 @@
 
 interface ImportMetaEnv {
   readonly VITE_LIFF_ID: string;
+  readonly VITE_LIFF_REDIRECT_URI: string;
+  readonly VITE_LINE_NOTIFY_CLIENT_ID: string;
+  readonly VITE_API_URL: string;
+  readonly VITE_RECORDS_PATH: string;
 }
 
 interface ImportMeta {
